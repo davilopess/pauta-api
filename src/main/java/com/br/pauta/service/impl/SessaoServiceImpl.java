70e49008015a54765a939dc051b636f2893ae069
davilopess <davi.lopes.amorim2014@hotmail.com>
2022-09-25 18:01:23 -0300

    [feat] Validação do voto na sessão do associado

--- src/main/java/com/br/pauta/service/impl/SessaoServiceImpl.java
+++ src/main/java/com/br/pauta/service/impl/SessaoServiceImpl.java
@@ -4,6 +4,8 @@ import com.br.pauta.document.Pauta;
 import com.br.pauta.document.Voto;
 import com.br.pauta.dto.SessaoDTO;
 import com.br.pauta.dto.VotoDTO;
+import com.br.pauta.exceptions.RequiredObjectIsNullException;
+import com.br.pauta.exceptions.ResourceNotFoundException;
 import com.br.pauta.repository.PautaRepository;
 import com.br.pauta.service.SessaoService;
 import com.br.pauta.service.ValidarCpf;
@@ -25,7 +27,7 @@ public class SessaoServiceImpl implements SessaoService {
     @Override
     public Pauta openSession(SessaoDTO sessaoDTO) {
         Pauta pauta = pautaRepository.findById(sessaoDTO.getPautaId())
-                .orElseThrow();
+                .orElseThrow(() -> new ResourceNotFoundException("Pauta nao informada para esse ID!"));
 
         if(pauta.getSessionOpen() == false){
             pauta.setMinutes(sessaoDTO.getMinutes() != null ? sessaoDTO.getMinutes() : pauta.getMinutes());
@@ -39,8 +41,9 @@ public class SessaoServiceImpl implements SessaoService {
     @Override
     public Voto addVoteToSession(VotoDTO votoDTO){
         Pauta pauta = pautaRepository.findById(votoDTO.getPautaId())
-                .orElseThrow();
+                .orElseThrow(() -> new ResourceNotFoundException("Pauta nao informada para esse ID!"));
 
+        validateVoteInSession(pauta, votoDTO.getAssociadoCpf());
         Voto voto = new Voto();
         voto.setVoto(votoDTO.getVoto());
         voto.setAssociadoCpf(votoDTO.getAssociadoCpf());
@@ -60,5 +63,11 @@ public class SessaoServiceImpl implements SessaoService {
         return pautaRepository.save(pauta);
     }
 
+    public boolean validateVoteInSession(Pauta pauta, String cpf){
+        if (pauta.sessionExpired()) throw new RequiredObjectIsNullException("Sessão expirada.");
+        if (pauta.cpfAlreadyVoted(cpf)) throw new RequiredObjectIsNullException("Voto já foi realizado por esse associado.");
+        if(!validarCpf.validar(cpf)) throw new RequiredObjectIsNullException("Associado não apode realizar o voto.");
+        return true;
+    }
 
 }
